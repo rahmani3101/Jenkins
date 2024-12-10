6c611ef2e19b018273a574694de05dbfcbd50e41
Mohammad Asad Rahmani <146559979+rahmani3101@users.noreply.github.com>
2024-12-10 23:23:33 +0530

    Create app.java

---
+++ app.java
@@ -0,0 +1,12 @@
+
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins World!");
+        System.out.println("Author : Mohammad Asad Rahmani \n email : asadrahmani3101@gmail.com ");
+    }
+
+    public String greet(String name) {
+        return "Hello, " + name + "!";
+    }
+}
